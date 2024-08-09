3f38813f9a37bfac86c415fa1ca215a08d773286
ElTheHuman <felixcristiano102938@gmail.com>
2024-08-10 02:03:58 +0700

    Add backup config

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ const root = createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <AuthProvider store={store}>
-            <BrowserRouter basename="/UniPal/">
+            <BrowserRouter basename="/UniPal_Backup/">
             {/* <HashRouter basename="/"> */}
                 <App />
             {/* </HashRouter> */}
